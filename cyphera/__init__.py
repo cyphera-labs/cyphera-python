f2bfd6714efc68f11348a5363bd29eb292dddf30
Leslie Gutschow <leslie.gutschow@horizondigital.dev>
2026-05-22 17:38:27 -0400

    chore: bump version to 0.0.1a7
    FF3-1 engine + strict NIST domain checks (BC-aligned) + FF1
    floating-point b-calculation fix where applicable.

--- cyphera/__init__.py
+++ cyphera/__init__.py
@@ -3,4 +3,4 @@ from cyphera.ff3 import FF3, FF31
 from cyphera.sdk import Cyphera, ALPHABETS
 
 __all__ = ["FF1", "FF3", "FF31", "Cyphera", "ALPHABETS"]
-__version__ = "0.0.1a4"
+__version__ = "0.0.1a7"
